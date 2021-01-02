42e1ef836e08b4d177d3bf4d963ca6352f517c92
Antfere <anthonyatanasov1@gmail.com>
2021-01-02 11:48:22 -0500

    mongoDB atlas

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 const DB_USER = 'Antfere';
-const PASSWORD = encodeURIComponent('@4^7JAMaNt *'); 
+const PASSWORD = encodeURIComponent('2467jamant'); 
 
 module.exports = {
-    mongoURI: `mongodb://${DB_USER}:${PASSWORD}@localhost:27017/music_list?authSource=admin`
+    mongoURI: `mongodb+srv://${DB_USER}:${PASSWORD}@cluster0.r0wtu.mongodb.net/music_list`
 }
