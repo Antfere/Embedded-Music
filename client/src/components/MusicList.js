19f6cd824fdedacd4f2c0e9ca23bd45b381ccd60
Antfere <anthonyatanasov1@gmail.com>
2021-01-23 11:51:04 -0500

    testing

--- client/src/components/MusicList.js
+++ client/src/components/MusicList.js
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { getItems, deleteItem, addItem, toggleItem } from "../actions/itemActions"
 import PropTypes from "prop-types";
 import AppNavbar from "./AppNavbar.js";
+import {Container} from "reactstrap";
 
 class MusicList extends Component {
 
@@ -45,6 +46,14 @@ class MusicList extends Component {
                     <AppNavbar toggle={this.toggle} items={items} />
                     <AddItem addItem={this.addItem}/>
                 </div>
+
+                <Container>
+                    <iframe allowfullscreen="allowfullscreen" frameborder="0" loading="eager" importance="high" src={"https://w.soundcloud.com/player/?visual=true&url=https://soundcloud.com/user-231592479/broke-for-free-something-elated&auto_play=true&show_artwork=true"}/>
+
+                    <iframe allowfullscreen="allowfullscreen" frameborder="0" loading="eager" importance="high" src={"https://w.soundcloud.com/player/?visual=true&url=https://soundcloud.com/negativexp/thotiana&auto_play=true&show_artwork=true"}/>
+
+                    <iframe allowfullscreen="allowfullscreen" frameborder="0" loading="eager" importance="high" src={"https://www.youtube.com/embed/9S3xK07SJSc?autoplay=1&rel=0&vq=144p"}/>
+                </Container>
 
                 <Items items={items} toggle={this.toggle} delItem={this.delItem}/>
 
